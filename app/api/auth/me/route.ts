cec222ab1d56259741970d5e9b9063cbbedfe78d
Arina Momot <arinamomot@gmail.com>
2025-01-16 00:47:32 +0100

    get/post user(s), get me

---
+++ app/api/auth/me/route.ts
@@ -0,0 +1,33 @@
+import { getServerSession } from 'next-auth/next';
+import { NextResponse } from 'next/server';
+import { authOptions } from '../../../../shared/constants/auth-options';
+import { prisma } from '../../../../prisma/prisma-client';
+
+export const dynamic = 'force-dynamic';
+
+export async function GET(req: any, res: any) {
+    try {
+        const user = await getServerSession(req, res, authOptions);
+
+        if (!user) {
+            return NextResponse.json(
+                { error: "Not authorized" },
+                { status: 401 })
+        }
+
+        const data = prisma.user.findUnique({
+            where: {
+                id: Number(user.user.id)
+            },
+            select: {
+                fullName: true,
+                email: true,
+                password: false
+            }
+        })
+        return NextResponse.json(data)
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({ error: '[USER_GET] Server error' }, { status: 500 });
+    }
+}
--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,19 +1,32 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "../../../prisma/prisma-client";
 
 export async function GET() {
-    const users = await prisma.user.findMany();
-    
-  return NextResponse.json({
-    users: users
-  });
+    try {
+        const users = await prisma.user.findMany();
+        return NextResponse.json({ users });
+    } catch (error) {
+        return NextResponse.json(
+            { error: "Failed to fetch users" },
+            { status: 500 }
+        );
+    }
 }
 
-// export function POST() {
-//   return {
-//     status: 200,
-//     body: {
-//       message: "Hello from the API!",
-//     },
-//   };
-// }
+export async function POST(req: NextRequest) {
+    try {
+        const data = await req.json();
+
+    const user = await prisma.user.create({
+        data
+    });
+
+    return NextResponse.json(user);
+    } catch (error) {
+        return NextResponse.json(
+            {error: "Failed to create user"},
+            {status: 500}
+        )
+    }
+    
+}
